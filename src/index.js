d57fd1198efa5927c915f3f36a47f261d8937ef6
Dmitri Kondramachine <dmkon@hotmail.com>
2018-01-12 10:13:02 -0500

    Component life cycle example.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ class App extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            timer: 0
+        };
+    }
+
+    componentWillMount() {
+        setInterval(()=>{
+            this.setState({timer: this.state.timer + 1});
+        }, 100);
     }
 
     render() {
         return (
             <div>
                 <h1>App component</h1>
-                <NewComponent text="Hello from App"/>
+                {this.state.timer < 50 ? <NewComponent text="Hello from App"/> : null}
+                <p>{this.state.timer}</p>
             </div>
         );
     }
--- src/new.js
+++ src/new.js
@@ -4,9 +4,23 @@ export default class NewComponent extends Component {
 
     constructor(props) {
         super(props);
+        console.log('Constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     }
 
     render() {
+        console.log('render');
         return (
             <div style={{'backgroundColor': 'red'}}>
                 <h3>New Component</h3>
