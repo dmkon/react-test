68bd96cf37908b03d6f956f58c29101d3ee14e77
Dmitri Kondramachine <dmkon@hotmail.com>
2018-01-12 14:41:52 -0500

    Route example.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 npm i
 npm install babel-preset-env --save-dev
 npm i eslint webpack-dev-server webpack eslint-plugin-react -g
-npm i react react-dom --save
+npm i react-router --save
+npm i react-router@3.x.x --save
 npm run dev
 Open `localhost:3000`
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Router, Route, browserHistory } from 'react-router';
 import ReactDOM from 'react-dom';
 import NewComponent from './new';
 
@@ -6,35 +7,21 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            array: [
-                {
-                    id: 1,
-                    text: 'Item 1'
-                },
-                {
-                    id: 2,
-                    text: 'Item 2'
-                },
-                {
-                    id: 3,
-                    text: 'Item 3'
-                }
-            ]
-        };
     }
 
     render() {
         return (
             <div>
                 <h1>App component</h1>
-                <NewComponent array={this.state.array}/>
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <Router history={browserHistory}>
+        <Route path={'/'} component={App} />
+        <Route path={'new'} component={NewComponent} />
+    </Router>,
     document.getElementById('app')
 );
